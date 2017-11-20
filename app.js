60b086620b223773be06e8709e9f8d2246a7581a
Juan Cuadra <jcb8793@gmail.com>
2017-11-20 16:18:11 +0100

    Login and Signup Setup

--- app.js
+++ app.js
@@ -1,53 +1,101 @@
-var express = require("express");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-const expressLayouts = require("express-ejs-layouts");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const flash = require("connect-flash");
-const MongoStore = require("connect-mongo")(session);
-const dotenv = require("dotenv");
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo')(session);
+const dotenv = require('dotenv');
 
-var index = require("./routes/index");
-var users = require("./routes/users");
-var auth = require("./routes/auth");
+const passportConfiguration = require('./helpers/passport')
+const index = require('./routes/index');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 
-var app = express();
+const app = express();
+
+// -- setup the app
+
+dotenv.config();
+
+// - session
+
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    secret: "some-string",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })
+);
+
+// - passport
+
+app.use(flash());
+passportConfiguration();
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// - database
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/game-up', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
+});
 
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// - views
+
+app.use(expressLayouts);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("layout", "index");
+
+// -- Middlewares
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", index);
-app.use("/", auth);
-app.use("/users", users);
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.status(404);
-  res.render("not-found");
+  res.render('not-found');
 });
 
 app.use(function(err, req, res, next) {
   // always log the error
-  console.error("ERROR", req.method, req.path, err);
+  console.error('ERROR', req.method, req.path, err);
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
     res.status(500);
-    res.render("error");
+    res.render('error');
   }
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,95 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require("bcrypt");
+const ensureLogin = require("connect-ensure-login");
+const router = express.Router();
+
+const User = require("../models/user").User;
+
+const bcryptSalt = 10;
+
+// router.use((req, res, next) => {
+//     if (req.user) {
+//       res.send("NICE!"); //@todo REPLACE WITH /GAME PAGE
+//       return;
+//     }
+//     next();
+//   });
+
+//-- login
+
+router.get("/login", function(req, res, next) {
+    const data = {
+      message: req.flash("error")
+    };
+    res.render("auth/login", data);
+  });
+  
+  router.post("/login",
+    passport.authenticate("local", {
+      successRedirect: "/auth/test",
+      failureRedirect: "/auth/login",
+      failureFlash: true,
+      passReqToCallback: true
+    })
+  );
 
 /* GET home page. */
 router.get('/signup', function (req, res, next) {
   res.render('auth/signup');
 });
+
+router.post('/signup', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username === ' || password === ') {
+      const data = {
+        message: 'Please provide username and password'
+      };
+      res.render('auth/signup', data);
+      return;
+    }
+  
+    User.findOne({ username }, 'username', (err, user) => {
+      if (err) {
+        next(err);
+        return;
+      }
+  
+      if (user) {
+        const data = {
+          message: 'The username already exists'
+        };
+        res.render('auth/signup', data);
+        return;
+      }
+  
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+  
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+  
+      newUser.save(err => {
+        if (err) {
+          next(err);
+          return;
+        }
+  
+        req.login(newUser, () => {
+          res.redirect('/auth/test');
+        });
+      });
+    });
+  });
+
+router.get('/test', ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render('games', { user: req.user });
+  });
 
 module.exports = router;
