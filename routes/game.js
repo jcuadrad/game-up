34d5d73c7fe0801a4b484eef62791586f4616bf4
Juan Cuadra <jcb8793@gmail.com>
2017-11-21 14:48:55 +0100

    Display Games in Main Map

--- routes/game.js
+++ routes/game.js
@@ -1,68 +1,63 @@
-const express = require("express");
-const ensureLogin = require("connect-ensure-login");
+const express = require('express');
+const ensureLogin = require('connect-ensure-login');
 const router = express.Router();
 
-const Game = require("../models/game").Game;
+const Game = require('../models/game').Game;
 
 /* GET home page. */
-<<<<<<< HEAD
-router.get("/new", (req, res, next) => {
-  res.render("game/new");
-});
-
-//NEW GAME
-
-// router.get("/:gameId", (req, res, next) => {
-//   res.render("game/edit", { title: "Edit Game" });
-// });
-
-router.post("/new/:id", (req, res, next) => {
-  const newGame = {
-    location: req.body.location,
-=======
-router.get('/',ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next) => {
+router.get('/', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next) => {
   res.render('games');
 });
 
-
-router.get("/new", ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next) => {
- res.render("edit");
+router.get('/new', ensureLogin.ensureLoggedIn('/auth/login'), (req, res, next) => {
+  res.render('edit');
 });
 
-router.post("/new", (req, res, next) => {
+router.post('/new', (req, res, next) => {
   let location = {
-    type: "Point",
+    type: 'Point',
     coordinates: [req.body.cityLat, req.body.cityLng]
   };
 
-  const newGame = new Game ({
+  const newGame = new Game({
     name: req.body.name,
     location: location,
->>>>>>> c5a0663267641970b80862ea405cffea30e15b63
     startTime: req.body.startTime,
     endTime: req.body.endTime,
     owner: req.user._id,
     playersNeeded: req.body.playersNeeded,
-    sport: req.body.sport,
+    sport: req.body.sport
   });
 
     // Save the game to the Database
-    newGame.save(error => {
-      if (error) {
-        console.log(error);
-      } else {
-        res.redirect("/");
-      }
-    });
+  newGame.save(error => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
-//GET GAME
-router.get("/game", (req, res, next) => {
-  res.render("/game");
+router.get('/games/json', (req, res, next) => {
+  Game.find((error, games) => {
+    if (error) {
+      console.log('error', error);
+      res.status(500).json({ error: 'FUUUUUUU!' });
+    } else {
+      res.json(games);
+      console.log(games);
+    }
+  });
 });
 
-router.post("/game", (req, res, next) => {
-  res.render("/game/:id/join");
+// GET GAME
+router.get('/game', (req, res, next) => {
+  res.render('/game');
+});
+
+router.post('/game', (req, res, next) => {
+  res.render('/game/:id/join');
 });
 
 module.exports = router;
